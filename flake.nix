b329c70ed9ab0515199485930cbe4a006be22c70
Antonio Yang <yanganto@gmail.com>
2022-04-09 13:00:26 +0800

    Add dependency

--- flake.nix
+++ flake.nix
@@ -1,18 +1,41 @@
 {
   description = "NP prototype";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-      packages.x86_64-linux.np = pkgs.rustPlatform.buildRustPackage rec {
-        name = "np";
-        src = self;
-        cargoSha256 = "sha256-oL9SxOvIQ6GOYvA0QxxQiUN4XNxRmaisLfmVNjkYsa8=";
-      };
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.np;
-    };
+  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        devRust = pkgs.rust-bin.nightly."2022-03-30".default;
+      in
+      with pkgs;
+      {
+        packages.x86_64-linux.np = pkgs.rustPlatform.buildRustPackage {
+          name = "np";
+          src = self;
+          cargoSha256 = "sha256-fxBzLGVloii31sUMIHTWSGeLZAuRBZvJ2/dJYo54ZqE=";
+          buildInputs = with pkgs; [
+            openssl
+            pkgconfig
+          ];
+        };
+        defaultPackage.x86_64-linux = self.packages.x86_64-linux.np;
+        devShell = mkShell {
+          buildInputs = [
+            devRust
+            openssl
+            pkgconfig
+          ];
+          RUST_BACKTRACE = 1;
+        };
+      }
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use near_jsonrpc_client::methods;
+use near_primitives::types::{BlockReference, Finality};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello world!");
+    Ok(())
 }
